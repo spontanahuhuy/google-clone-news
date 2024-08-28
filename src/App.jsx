83652a9da8a0c62d28926d764bec8592a1deec49
spontanahuhuy <akmalzidane21@gmail.com>
2024-08-28 14:18:47 +0700

    add menubar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Navbar from "./components/Navbar";
+import Menubar from "./components/Menubar";
 
 function App() {
   return (
     <div className="bg-gray-100 min-h-screen>">
       <Navbar />
+      <Menubar />
     </div>
   )
 }
---
+++ src/components/Menubar.jsx
@@ -0,0 +1,17 @@
+const Menubar = () => {
+    return (
+        <div className="bg-white flex items-center justify-center shadow-md p-2 w-screen">
+            <h1 className="ml-4 font-medium text-gray-500 text-base hover:text-black cursor-pointer">Home</h1>
+            <h1 className="ml-7 font-medium text-gray-500 text-base hover:text-black cursor-pointer">For You</h1>
+            <h1 className="ml-7 font-medium text-gray-500 text-base hover:text-black cursor-pointer">Following</h1>
+            <h1 className="ml-7 text-gray-200">|</h1>
+            <h1 className="ml-7 font-medium text-gray-500 text-base hover:text-black cursor-pointer">Indonesia</h1>
+            <h1 className="ml-7 font-medium text-gray-500 text-base hover:text-black cursor-pointer">World</h1>
+            <h1 className="ml-7 font-medium text-gray-500 text-base hover:text-black cursor-pointer">Local</h1>
+            <h1 className="ml-7 font-medium text-gray-500 text-base hover:text-black cursor-pointer">Entertainment</h1>
+            <h1 className="ml-7 font-medium text-gray-500 text-base hover:text-black cursor-pointer">Sports</h1>
+        </div>
+    );
+}
+
+export default Menubar;
